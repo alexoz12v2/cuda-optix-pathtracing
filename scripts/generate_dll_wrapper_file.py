7c3bcba23bb7e80bbd8473508951ba53ca120762
alessio <oboken1974@hotmail.it>
2025-03-24 20:57:56 +0100

    python scripts now supports using platform stuff to load libraries. Useless?

--- scripts/generate_dll_wrapper_file.py
+++ scripts/generate_dll_wrapper_file.py
@@ -1,12 +1,14 @@
 from collections import namedtuple
 import json
-from pprint import pprint
 import re
 import subprocess
 import platform
 from argparse import ArgumentParser
 from pathlib import Path
 from typing import List, Tuple
+
+
+MethodData = namedtuple("MethodData", "name type latest_version")
 
 
 def get_exports_windows(dll_path: Path) -> List[str]:
@@ -52,7 +54,9 @@ def get_exports_linux(so_path: Path) -> List[str]:
 def filter_latest_versions(exports, version_pattern):
     """Filters out older versions of functions based on a version pattern."""
     if not version_pattern:
-        return {export: export for export in exports}  # Return as a dictionary {base_name: latest_version}
+        return {
+            export: export for export in exports
+        }  # Return as a dictionary {base_name: latest_version}
 
     version_regex = re.compile(version_pattern.replace("{n}", r"(\d+)"))
     latest_versions = {}
@@ -60,13 +64,15 @@ def filter_latest_versions(exports, version_pattern):
     for export in exports:
         match = version_regex.search(export)
         if match:
-            base_name = export[:match.start()]
+            base_name = export[: match.start()]
             version = int(match.group(1))
 
             # Check if base_name is in latest_versions and safely extract its version
             if base_name in latest_versions:
                 existing_match = version_regex.search(latest_versions[base_name])
-                existing_version = int(existing_match.group(1)) if existing_match else -1
+                existing_version = (
+                    int(existing_match.group(1)) if existing_match else -1
+                )
             else:
                 existing_version = -1  # Default to -1 so any valid version is higher
 
@@ -79,10 +85,30 @@ def filter_latest_versions(exports, version_pattern):
     return latest_versions  # Return a dictionary { base_name: latest_version_name }
 
 
-def generate_loader(library_path, includes, version_pattern, latest_only, json_file, header_name: str | None) -> Tuple[str, str]:
-    """Generates C++ code for dynamically loading functions from a shared library."""
-    header_string = ""
-    implementation_string = ""
+def write_type_declarations_and_populate_method_data(
+    json_file, exports, data, header_string
+):
+    if json_file is not None:
+        with json_file.open(encoding="UTF-8") as source:
+            tmap = json.load(source)
+            tmap = {k.lower(): v for k, v in tmap.items()}  # Case-insensitive mapping
+    else:
+        tmap = {}
+
+    for base_name, latest_version in exports.items():
+        # Use tmap lookup if available, otherwise default to void(*)()
+        func_ptr_type = tmap.get(
+            base_name.lower(), "void(*)()"
+        )  # Default to function pointer
+        t_sig = (
+            f"using {latest_version}_t = {func_ptr_type};"  # Using the latest version
+        )
+
+        header_string[0] += f"    {t_sig}\n"
+        data.append(MethodData(base_name, t_sig.split(" ")[1], latest_version))
+
+
+def prepare_library_population(library_path, version_pattern, latest_only):
     is_windows = platform.system() == "Windows"
     exports = (
         get_exports_windows(library_path)
@@ -94,10 +120,60 @@ def generate_loader(library_path, includes, version_pattern, latest_only, json_f
         exports = filter_latest_versions(exports, version_pattern)
 
     library_name = Path(library_path).name
-    library_class_name = library_name.replace('-', '_').capitalize()[:library_name.rfind('.')]
+    library_class_name = library_name.replace("-", "_").capitalize()[
+        : library_name.rfind(".")
+    ]
+    return exports, library_name, library_class_name
 
-    implementation_string += f"#include \"{header_name}\"\n\n"
-    implementation_string += """#ifdef _WIN32
+
+def append_class_header_termination(header_string, data):
+    header_string[0] += "\n  public:\n"
+    for methodData in data:
+        header_string[0] += f"    {methodData.type} {methodData.name};\n"
+
+    header_string[0] += "};\n"
+
+
+def append_class_header_preamble(includes, header_string, exports, library_class_name):
+    header_string[0] += "\n" + "".join(f'#include "{inc}"\n' for inc in includes) + "\n"
+
+    # Add the #undef macros
+    for base_name in exports.keys():
+        header_string[0] += f"#ifdef {base_name}\n#undef {base_name}\n#endif\n"
+
+    header_string[0] += f"""
+class {library_class_name}LibraryFunctions {{
+  public:
+"""
+
+
+def generate_loader(
+    library_path,
+    includes,
+    version_pattern,
+    latest_only,
+    json_file,
+    header_name: str | None,
+) -> Tuple[str, str]:
+    """Generates C++ code for dynamically loading functions from a shared library."""
+    header_string = ["#pragma once\n"]
+    implementation_string = [""]
+    exports, library_name, library_class_name = prepare_library_population(
+        library_path, version_pattern, latest_only
+    )
+    data: List[MethodData] = list()
+
+    append_class_header_preamble(includes, header_string, exports, library_class_name)
+    write_type_declarations_and_populate_method_data(
+        json_file, exports, data, header_string
+    )
+    append_class_header_termination(header_string, data)
+    header_string[0] += (
+        f"bool load{library_class_name}Functions({library_class_name}LibraryFunctions* funcList);\n"
+    )
+
+    implementation_string[0] += f'#include "{header_name}"\n\n'
+    implementation_string[0] += """#ifdef _WIN32
 #include <windows.h>
 #else
 #include <dlfcn.h>
@@ -106,14 +182,7 @@ def generate_loader(library_path, includes, version_pattern, latest_only, json_f
 #include <cstdlib>
 
 """
-
-    header_string += "\n" + "".join(f'#include "{inc}"\n' for inc in includes) + "\n"
-
-    # Add the #undef macros
-    for base_name in exports.keys():
-        header_string += f"#ifdef {base_name}\n#undef {base_name}\n#endif\n"
-
-    implementation_string += f"""
+    implementation_string[0] += f"""
 
 static void* libraryHandle = nullptr;
 
@@ -148,46 +217,76 @@ static void* LoadLibraryFunc(const char* func_name) {{
 #endif
 }}
 """
-    header_string += f"""
-class {library_class_name}LibraryFunctions {{
-  public:
-"""
-    MethodData = namedtuple("MethodData", "name type latest_version")
-    data: List[MethodData] = list()
 
-    if json_file is not None:
-        with json_file.open(encoding="UTF-8") as source:
-            tmap = json.load(source)
-            tmap = {k.lower(): v for k, v in tmap.items()}  # Case-insensitive mapping
-    else:
-        tmap = {}
-
-    for base_name, latest_version in exports.items():
-        # Use tmap lookup if available, otherwise default to void(*)()
-        func_ptr_type = tmap.get(base_name.lower(), f"void(*)()")  # Default to function pointer
-        t_sig = f"using {latest_version}_t = {func_ptr_type};"  # Using the latest version
-
-        header_string += f"    {t_sig}\n"
-        data.append(MethodData(base_name, t_sig.split(" ")[1], latest_version))
-
-    header_string += "\n  public:\n"
-    for methodData in data:
-        header_string += f"    {methodData.type} {methodData.name};\n"
-
-    header_string += "};\n"
-    header_string += f"bool load{library_class_name}Functions({library_class_name}LibraryFunctions* funcList);\n"
-
-    implementation_string += f"""
+    implementation_string[0] += f"""
 bool load{library_class_name}Functions({library_class_name}LibraryFunctions* funcList)
 {{
 """
     for methodData in data:
-        implementation_string += f"    funcList->{methodData.name} = reinterpret_cast<{library_class_name}LibraryFunctions::{methodData.type}>(LoadLibraryFunc(\"{methodData.latest_version}\"));\n"
-        implementation_string += f"    if (!funcList->{methodData.name}) {{ return false; }}\n"
-    
-    implementation_string += "    return true;\n}\n"
+        implementation_string[0] += (
+            f'    funcList->{methodData.name} = reinterpret_cast<{library_class_name}LibraryFunctions::{methodData.type}>(LoadLibraryFunc("{methodData.latest_version}"));\n'
+        )
+        implementation_string[0] += (
+            f"    if (!funcList->{methodData.name}) {{ return false; }}\n"
+        )
 
-    return (header_string, implementation_string)
+    implementation_string[0] += "    return true;\n}\n"
+
+    return (header_string[0], implementation_string[0])
+
+
+def platform_generate_loader(
+    library_path: Path,
+    includes: List[str],
+    version_pattern: str,
+    latest_only: bool,
+    json_file: Path,
+    header_name: str | None,
+) -> Tuple[str, str]:
+    header_string = ['#pragma once\n#include "platform/platform-utils.h"\n\n']
+    implementation_string = [f'#include "{header_name}"\n\n']
+    exports, library_name, library_class_name = prepare_library_population(
+        library_path, version_pattern, latest_only
+    )
+    data: List[MethodData] = list()
+    search_path_parts = library_path.parent.parts
+    search_composition = " / " + " / ".join(
+        f'"{s}"' for i, s in enumerate(search_path_parts) if i > 0
+    )
+    search_drive = '"' + library_path.drive + '"'
+
+    append_class_header_preamble(includes, header_string, exports, library_class_name)
+    write_type_declarations_and_populate_method_data(
+        json_file, exports, data, header_string
+    )
+    header_string[0] += "void* m_library;\n"
+    append_class_header_termination(header_string, data)
+    header_string[0] += (
+        f"\nbool load{library_class_name}Functions(dmt::LibraryLoader const& loader, {library_class_name}LibraryFunctions* funcList);\n"
+    )
+
+    implementation_string[0] += (
+        f"bool load{library_class_name}Functions(dmt::LibraryLoader const& loader, {library_class_name}LibraryFunctions* funcList) {{\n"
+    )
+    implementation_string[0] += "    using std::string_view_literals;\n\n"
+    implementation_string[0] += (
+        f"    dmt::os::Path path = dmt::os::Path::root({search_drive}){search_composition};\n"
+    )
+    implementation_string[0] += (
+        f'    funcList->m_library = loader.loadLibrary("{library_name}"sv, true, &path);\n'
+    )
+    implementation_string[0] += "    if (!funcList->m_library) {{ return false; }}\n"
+    for methodData in data:
+        implementation_string[0] += (
+            f'    funcList->{methodData.name} = reinterpret_cast<{library_class_name}LibraryFunctions::{methodData.type}>(dmt::os::lib::getFunc(funcList->m_library, "{methodData.latest_version}"));\n'
+        )
+        implementation_string[0] += (
+            f"    if (!funcList->{methodData.name}) {{ return false; }}\n"
+        )
+
+    implementation_string[0] += "    return true;\n}\n"
+
+    return (header_string[0], implementation_string[0])
 
 
 # TODO add clang-format after generation
@@ -225,25 +324,16 @@ def main():
         "--json-type-mapping",
         type=str,
         default="",
-        help="Json which maps an exported symbol to a typedef/using declaration (fallback to void*)"
+        help="Json which maps an exported symbol to a typedef/using declaration (fallback to void*)",
     )
-    parser.add_argument(
-        "-hf",
-        "--header-file",
-        type=str,
-        required=True
-    )
-    parser.add_argument(
-        "-cpp",
-        "--cpp-file",
-        type=str,
-        required=True
-    )
-    # TODO 
+    parser.add_argument("-hf", "--header-file", type=str, required=True)
+    parser.add_argument("-cpp", "--cpp-file", type=str, required=True)
+    # TODO
     parser.add_argument(
         "-up",
         "--use-platform",
         action="store_true",
+        default=False,
         help="Produce a translation unit which depends on the dmt-platform cmake target",
     )
 
@@ -252,7 +342,7 @@ def main():
     if not args.library.exists():
         print(f"Error: Library file '{args.library}' not found.")
         return
-    
+
     header_file = Path(args.header_file)
     cpp_file = Path(args.cpp_file)
     header_name = header_file.name
@@ -263,8 +353,18 @@ def main():
     else:
         json_file = None
 
-    header_string, implementation_string = generate_loader(
-        args.library, args.includes, args.version_pattern, args.latest_only, json_file, header_name
+    if args.use_platform:
+        generate_loader_function = platform_generate_loader
+    else:
+        generate_loader_function = generate_loader
+
+    header_string, implementation_string = generate_loader_function(
+        args.library,
+        args.includes,
+        args.version_pattern,
+        args.latest_only,
+        json_file,
+        header_name,
     )
 
     with cpp_file.open("w", encoding="utf-8") as f:
